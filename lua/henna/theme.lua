97a5fc447222268edc1de31dc97617e9a67a1074
timmy <timhansher@gmail.com>
2025-02-02 06:17:18 -0500

    wildmenu again

--- lua/henna/theme.lua
+++ lua/henna/theme.lua
@@ -130,7 +130,7 @@ M.base = {
 
     WarningMsg = { fg = colors.orange_wr, bg = colors.none },
     Whitespace = { fg = colors.non_text },
-    WildMenu = { fg = colors.fg, bg = colors.vsplit_bg },
+    WildMenu = { fg = colors.fg, bg = colors.pink },
     lCursor = { fg = colors.bg, bg = colors.pink },
 
 
