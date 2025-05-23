0e96ad8de203feb2b583427b0fb432171f73c404
timmy <timhansher@gmail.com>
2025-05-22 20:27:28 -0400

    test

--- lua/henna/theme.lua
+++ lua/henna/theme.lua
@@ -159,7 +159,7 @@ M.base = {
   BlinkCmpMenuBorder = { fg = colors.code, bg = colors.code },
   BlinkCmpDoc = { fg = colors.fg, bg = colors.code },
   BlinkCmpDocBorder = { fg = colors.code, bg = colors.code },
-  FzfLuaNormal = { fg = colors.fg, bg = colors.code },
+  FzfLuaBorder = { fg = colors.vsplit_by  }
 
 
     -----------------------------------------
