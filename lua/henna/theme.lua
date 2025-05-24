ba2e023110248e4ca5f5e84e7261ebd43745ca37
timmy <timhansher@gmail.com>
2025-05-23 21:37:40 -0400

    test

--- lua/henna/theme.lua
+++ lua/henna/theme.lua
@@ -162,6 +162,9 @@ M.base = {
         fg = colors.vsplit_bg,
         bg = colors.vsplit_bg,
     },
+    SnacksPickerBoxTitle = { bg = colors.red_err },
+    SnacksPickerPreviewTitle = { fg = colors.yellow },
+    AvanteTitle = { fg = colors.orange },
 
     -----------------------------------------
     --        end Editors settings
